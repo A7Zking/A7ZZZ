0eea6a263158beda155e0328af6cf2f6840c94ab
A7Zking <56368745+A7Zking@users.noreply.github.com>
2019-10-19 14:16:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -121,7 +121,7 @@ member.guild.fetchInvites().then(guildInvites => {
       var Canvas = require('canvas')
       var jimp = require('jimp')
       
-      const w = ['./w11.png'];
+      const w = ['./w1.png'];
       
               let Image = Canvas.Image,
                   canvas = new Canvas(400, 200),
